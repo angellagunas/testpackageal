ff2740318a0d0440cae96fdb4e883e079a1527a1
angel lagunas <al@vincoorbis.com>
2017-01-11 20:14:22 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'testpackage',
+  packages = ['testpackageal'], # this must be the same as the name above
+  version = '0.1',
+  description = 'This is my first package.',
+  author = 'Angel Lagunas',
+  author_email = 'angel.david.lagunas@gmail.com',
+  url = 'https://github.com/angellagunas/testpackageal', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
